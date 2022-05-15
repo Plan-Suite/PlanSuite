ee6952573d36c953b4c8422ed966e9f9052d9876
Sean McElholm <seanmcelholm@gmail.com>
2022-05-15 18:31:49 +0100

    Website version in footer

---
+++ PlanSuite/Utility/WebsiteVersion.cs
@@ -0,0 +1,29 @@
+﻿namespace PlanSuite.Utility
+{
+    /// <summary>
+    /// Website Version
+    /// </summary>
+    public static class WebsiteVersion
+    {
+        /// <summary>
+        /// Increment this when we add/change major features, UI overhauls or major backend changes
+        /// </summary>
+        public const int VERSION_MAJOR = 1;
+
+        /// <summary>
+        /// Increment this when we add/change minor features, incremental UI changes or backend changes
+        /// </summary>
+        public const int VERSION_MINOR = 1;
+
+        // Increment this when we fix bugs, update dependencies or other non-noticable features
+        public const int VERSION_PATCH = 0;
+
+        public static string Version
+        {
+            get
+            {
+                return $"{VERSION_MAJOR}.{VERSION_MINOR}.{VERSION_PATCH}";
+            }
+        }
+    }
+}
