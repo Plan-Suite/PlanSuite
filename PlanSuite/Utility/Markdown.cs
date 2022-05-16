86a540ebe2bf87b56160eab0d6112a3a0c36b70d
Sean McElholm <seanmcelholm@gmail.com>
2022-05-16 14:17:27 +0100

    Markdown utility class

---
+++ PlanSuite/Utility/Markdown.cs
@@ -0,0 +1,18 @@
+﻿namespace PlanSuite.Utility
+{
+    public static class Markdown
+    {
+        /// <summary>
+        /// Parse Markdown
+        /// </summary>
+        /// <param name="markdown">Markdown string to parse</param>
+        /// <returns>Markdown string parsed as HTML</returns>
+        public static string Parse(string markdown)
+        {
+            string parsedMd = Markdig.Markdown.ToHtml(markdown);
+            Console.WriteLine(parsedMd);
+            return parsedMd;
+        }
+    }
+
+}
