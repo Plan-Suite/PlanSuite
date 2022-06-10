96df1896eae8d8af30a5f281d40295b84ab5ebfc
Sean McElholm <seanmcelholm@gmail.com>
2022-06-11 00:07:00 +0100

    Minify scripts on build

--- PlanSuite/wwwroot/js/site.js
+++ PlanSuite/wwwroot/js/site.js
@@ -1,14 +1,4 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
-
-$(document).ready(function () {
-    $("#sortable").sortable();
-    $("#sortable").disableSelection();
-})
-
-function isBlank(str) {
+﻿function isBlank(str) {
     return (!!!str || /^\s*$/.test(str));
 }
 
