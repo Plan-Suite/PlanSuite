847f858a05078c649bdf9842f4d5c0b30a004245
Sean McElholm <seanmcelholm@gmail.com>
2022-06-17 03:03:45 +0100

    Home/Index JS to TS

---
+++ PlanSuite/scripts/home/index.ts
@@ -0,0 +1,40 @@
+﻿$(function () {
+
+    $("#MyProjects").children().each(function () {
+        var element = $(this);
+        if (element.attr("id").startsWith("OwnedProject_")) {
+            var id: number = element.children("#projectId").val() as number;
+            var name: string = element.children("#projectName").val() as string;
+            var desc: string = element.children("#projectDesc").val() as string;
+            var date: string = element.children("#projectDate").val() as string | "";
+
+            $(`#passButtonInfoBtn_${id}`).on("click", function () { passButtonInfo(id, name, desc, date); });
+            $(`#passDeleteButtonInfoBtn_${id}`).on("click", function () { passDeleteButtonInfo(id, name); });
+        }
+    });
+});
+
+function passButtonInfo(dbId, name, desc, date) {
+    $("#editProjectLabel").html(`<strong>Edit:</strong> ${name}`);
+    $('#editProjName').val(name);
+    $('#editProjDesc').val(desc);
+    $('#editProjDate').val(date); // i have no fking clue how to get this working :(
+    $('#EditProject_Id').val(dbId);
+}
+
+function passDeleteButtonInfo(dbId, name) {
+    $('#deleteProjectLabel').html(`<strong>Delete:</strong> ${name}`);
+    $('#DeleteProject_Name').val(name);
+    $('#DeleteProject_Id').val(dbId);
+}
+
+function onDeleteInput() {
+    const button = $('#deleteButton');
+    const projName = $('#DeleteProject_Name').val();
+    if ($('#confirmDeleteProjName').val() == projName) {
+        button.attr('disabled', 0);
+    }
+    else {
+        button.attr('disabled', 1);
+    }
+}
