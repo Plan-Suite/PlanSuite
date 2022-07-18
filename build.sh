f0bd3a271aeb95e14f8da7dcd3f24499b08d4ac4
Sean McElholm <seanmcelholm@gmail.com>
2022-07-18 09:23:06 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,14 +3,43 @@ today=`date +%d%b%y_%H%M`
 SQLFILE=~/backup/backup
 DATABASE=plan_suite
 USER=plan-suite
+OUTPUT=~/build/Output
+GIT=git@github.com:Plan-Suite/PlanSuite.git
+
+echo "-> Downloading latest build"
+cd ~/build
+rm -rf ~/build/plan-suite
+git clone ${GIT}
+cd ..
+
+echo "-> Cleaning old build"
+rm -rf ${OUTPUT}
+mkdir -p ${OUTPUT}
 
 echo "-> Building production"
-if dotnet publish ~/build/plan-suite/PlanSuite/PlanSuite.csproj --configuration Release --runtime linux-x64 --self-contained --output ~/build/Output
+cd ~/build/plan-suite/PlanSuite
+npm i
+cd
+
+if dotnet publish ~/build/plan-suite/PlanSuite/PlanSuite.csproj --configuration Release --runtime linux-x64 --self-contained --output ${OUTPUT}
 then
-    cp ~/build/appsettings.json Output/appsettings.json
-    cp -R ~/build/Output/* /var/www/plansuite
+    echo "-> Copying files"
+    cp ~/build/appsettings.json ~/build/Output/appsettings.json
+    cp -R ${OUTPUT}/* /var/www/plansuite
+
+    echo "-> Backing up database"
     mysqldump --defaults-file=~/.my.cnf -u ${USER} ${DATABASE}|gzip > ${SQLFILE}-$today.sql.gz
+
+    echo "-> Applying new database changes"
+    cd ~/build/plan-suite/PlanSuite
+    cp ~/build/appsettings.json ~/build/plan-suite/PlanSuite/appsettings.json
+    dotnet ef migrations bundle --self-contained -r linux-x64
+   ~/build/plan-suite/PlanSuite/efbundle
+
+    echo "-> Restarting website"
     sudo service plansuite restart
+
+    echo "-> Done"
 else
     echo "-> Failed to build production build due to .NET errors."
     exit 1
